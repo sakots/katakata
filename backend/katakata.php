ac2633d2234629041f48d1eb940c0f017fb77947
sakots <sakots8000@gmail.com>
2026-01-31 16:10:45 +0900

    init

---
+++ backend/katakata.php
@@ -0,0 +1,8 @@
+<?
+//-------------
+// Katakata
+//-------------
+// A simple note-taking web application
+// Backend file
+//-------------
+header('Content-Type: application/json');
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>katakata</h1>
+      <div>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
