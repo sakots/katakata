2335b3c6f788fa3e38b8321d1ac4a6aa88a4d712
sakots <sakots8000@gmail.com>
2026-02-26 11:00:55 +0900

    APP

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,11 @@
-import { useQuery } from '@tanstack/react-query'
-import axios from 'axios'
 import './styles/App.css'
+import DirectoryTree from './components/DirectoryTree'
 
 function App() {
-
-  interface Post {
-    id: number
-    title: string
-  }
-
-  const { data } = useQuery<Post[]>({
-  queryKey: ["posts"],
-  queryFn: () => axios.get(import.meta.env.VITE_API_URL + "/issues").then(res => res.data)
-})
-
   return (
     <>
       <h1>katakata</h1>
-      <div>
-        <ul>
-          {data?.map(post => (
-            <li key={post.id}>{post.title}</li>
-          ))}
-        </ul>
-      </div>
+      <DirectoryTree />
     </>
   )
 }
